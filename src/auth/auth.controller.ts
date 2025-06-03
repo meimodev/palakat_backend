82c7c20b7a51f14e92cfb3dabe3be67b69f5f76f
MikelNoppo <mikelnoppo03@gmail.com>
2025-06-03 16:40:55 +0800

    feat(auth): add basic user phone validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  
+  @Post('validate-user')
+  async validateUser(phone: string) {
+    return this.authService.validateUser(phone);
+  }
 }
