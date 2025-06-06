d486133d5683b3c5f292bd4ca3c2193bccc886f6
meimo <59273253+meimodev@users.noreply.github.com>
2025-06-06 12:04:45 +0800

    apply prefix for base url, alter readme

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
 
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  await app.listen(3000);
 }
+
 bootstrap();
