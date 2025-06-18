19c59c03d0dfa2c32434d3573a3b7a9f180cc308
Miracle Sumajow <miraclesumajow32@gmail.com>
2025-06-18 23:46:02 +0800

    end point for retrieving account data

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,5 +1,7 @@
 import {
   Controller,
+  Get,
+  Query,
   Post,
   Body,
   UseGuards,
@@ -14,7 +16,13 @@ import { AuthGuard } from '@nestjs/passport';
 @Controller('account')
 @UseGuards(AuthGuard('jwt'))
 export class AccountController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(private readonly accountService: AccountService) { }
+
+  @Get()
+  async findOne(@Query('phone') phone?: string, @Query('id') id?: string) {
+    return this.accountService.findOne({ phone, id });
+  }
+
 
   @Post()
   create(@Body() createAccountDto: Prisma.AccountCreateInput) {
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -4,7 +4,44 @@ import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AccountService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
+
+  async findOne(params: { phone?: string; id?: string }) {
+    const { phone, id } = params;
+
+    if (!phone && !id) {
+      badRequest('Either phone or id must be provided.');
+    }
+
+    try {
+      const account = await this.prisma.account.findFirst({
+        where: {
+          ...(phone ? { phone } : {}),
+          ...(id && { id: parseInt(id) }),
+        },
+        include: {
+          membership: {
+            include: {
+              column: true,
+              church: true,
+            },
+          },
+        },
+      });
+
+      if (!account) {
+        notFound('Account not found');
+      }
+
+      return {
+        message: HttpStatus.OK,
+        data: account,
+      };
+    } catch (error) {
+      console.error('Error finding account:', error);
+      internalServerError('Error retrieving account');
+    }
+  }
 
   async create(createAccountDto: Prisma.AccountCreateInput) {
     const account = await this.prisma.account.create({
