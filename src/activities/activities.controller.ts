9f280952e00cf7d753668f7fb215034c08d632df
MikelNoppo <mikelnoppo03@gmail.com>
2025-07-07 22:53:19 +0800

    refactor: remove Activities module, controller, and service

--- src/activities/activities.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ActivitiesService } from './activities.service';
-
-@Controller('activities')
-export class ActivitiesController {
-  constructor(private readonly activitiesService: ActivitiesService) {}
-}
