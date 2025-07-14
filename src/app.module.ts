82037c94938b5b6a9ff6a077c405e06fa5047874
meimo <59273253+meimodev@users.noreply.github.com>
2025-07-14 20:45:49 +0800

    add placeholder for helper class

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AccountModule } from './account/account.module';
 import { PrismaExceptionFilter } from './exception.filter';
 import { MembershipModule } from './membership/membership.module';
 import { ActivitiesModule } from './activity/activity.module';
+import { HelperService } from '../common/helper/helper.service';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { ActivitiesModule } from './activity/activity.module';
     ActivitiesModule,
   ],
   controllers: [AppController],
-  providers: [AppService, Logger, PrismaExceptionFilter],
+  providers: [AppService, Logger, PrismaExceptionFilter, HelperService],
+  exports: [HelperService],
 })
 export class AppModule {}
