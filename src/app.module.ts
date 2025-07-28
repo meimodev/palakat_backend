c623923b04bdfa07cf487e4d26ea566ef71f5473
Miracle Sumajow <miraclesumajow32@gmail.com>
2025-07-28 08:57:17 +0800

    feat: add SongPart module, controller, and service for managing song parts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ActivitiesModule } from './activity/activity.module';
 import { ChurchModule } from './church/church.module';
 import { HelperService } from '../common/helper/helper.service';
 import { SongModule } from './song/song.module';
+import { SongPartModule } from './song-part/song-part.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { SongModule } from './song/song.module';
     ActivitiesModule,
     ChurchModule,
     SongModule,
+    SongPartModule,
   ],
   controllers: [AppController],
   providers: [AppService, Logger, PrismaExceptionFilter, HelperService],
---
+++ src/song-part/song-part.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class SongPartService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async create(songId: number, createSongPartDto: Prisma.SongPartCreateInput) {
+        const data = {
+            ...createSongPartDto,
+            song: { connect: { id: songId } },
+        };
+        const created = await this.prisma.songPart.create({
+            data,
+        });
+        return {
+            message: 'OK',
+            data: created,
+        };
+    }
+
+    async findAll(songId?: number) {
+        const where: Prisma.SongPartWhereInput = {};
+
+        if (songId) {
+            where.songId = songId;
+        }
+        const parts = await this.prisma.songPart.findMany({
+            where,
+        });
+        return { message: `Ditemukan ${parts.length} bagian lagu.`, data: parts };
+    }
+
+    async findOne(id: number) {
+        return this.prisma.songPart.findUnique({ where: { id } });
+    }
+
+    async update(id: number, data: Prisma.SongPartUpdateInput) {
+        return this.prisma.songPart.update({ where: { id }, data });
+    }
+
+    async delete(id: number) {
+        return this.prisma.songPart.delete({ where: { id } });
+    }
+}
