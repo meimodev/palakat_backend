2547817a00e9e85b0afbd1c765de6e8b8e824704
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-13 01:17:26 +0800

    feat: Implement pagination decorator, interceptor, and utility functions for enhanced data handling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exception.filter';
+import { PaginationInterceptor } from '../common/pagination/pagination.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
   });
 
   app.useGlobalFilters(prismaExceptionFilter);
+  app.useGlobalInterceptors(new PaginationInterceptor());
 
   await app.listen(process.env.PORT || 3000);
 }
