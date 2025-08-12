49fd2afbfd21eb1b0005df46bac556ee4055cfe5
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-13 02:05:59 +0800

    feat: Refactor ColumnController and ColumnService to implement pagination for getColumns method

--- src/column/column.controller.ts
+++ src/column/column.controller.ts
@@ -13,7 +13,8 @@ import {
 import { ColumnService } from './column.service';
 import { Prisma } from '@prisma/client';
 import { AuthGuard } from '@nestjs/passport';
-
+import { Pagination } from 'common/pagination/pagination.decorator';
+import { PaginationParams } from 'common/pagination/pagination.types';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('column')
@@ -21,9 +22,16 @@ export class ColumnController {
   constructor(private readonly columnService: ColumnService) {}
 
   @Get()
-  async getColumns(@Query('churchId') churchId?: string) {
+  async getColumns(
+    @Pagination() pagination: PaginationParams,
+    @Query('churchId') churchId?: string,
+  ) {
     const ChurchId = churchId ? parseInt(churchId, 10) : undefined;
-    return this.columnService.getColumns(ChurchId);
+    return this.columnService.getColumns({
+      churchId: ChurchId,
+      skip: pagination.skip ?? 0,
+      take: pagination.take ?? 20,
+    });
   }
 
   @Get(':id')
