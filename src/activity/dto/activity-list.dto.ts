8b2f30a52682ea05979808d91c39cfba0f463f8c
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-17 11:42:48 +0800

    feat: Implement pagination DTOs for various query classes

---
+++ src/activity/dto/activity-list.dto.ts
@@ -0,0 +1,36 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Min, ValidateIf } from 'class-validator';
+import { PaginationQueryDto } from '../../../common/pagination/pagination.dto';
+
+export class ActivityListQueryDto extends PaginationQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  membershipId?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  churchId?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  columnId?: number;
+
+  @IsOptional()
+  @Type(() => Date)
+  startTimestamp?: Date;
+
+  @IsOptional()
+  @Type(() => Date)
+  endTimestamp?: Date;
+
+  @ValidateIf(o => o.startTimestamp && o.endTimestamp && o.startTimestamp > o.endTimestamp)
+  get invalidRange() {
+    throw new Error('startTimestamp must be before or equal to endTimestamp');
+  }
+}
